c4291d13afdbe3aaa13f2370efd8d982ffa89e67
alvinIyke <alvin.uchey@yahoo.com>
2024-10-04 15:51:00 +0100

    new interactive quiz

---
+++ quiz.js
@@ -0,0 +1,10 @@
+function checkAnswer () {
+    const correctAnswer = "4";   //define the correct answer
+    const userAnswer = document.querySelector('input[name = "quiz"]: checked').value;
+}
+if ( userAnswer === correctAnswer) {
+    document.getElementById("feedback").textContent = "Correct! Well done.";
+} else {
+    document.getElementById("feedback").textContent = "That's incorrect. Try again!";
+}
+document.getElementById("submit-answer"). addEventListener("click", checkAnswer);
